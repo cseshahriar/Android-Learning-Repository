9f4e22eda5dbccd796935dc060e58ce909aca7ae
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-05-04 20:57:29 +0600

    EditText and Button

---
+++ EditText_Button/app/src/main/java/com/example/edittext_button/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.edittext_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submit(View view) {
+        Toast.makeText(this, "Button Clicked Successfull", Toast.LENGTH_SHORT).show();
+     }
+}
