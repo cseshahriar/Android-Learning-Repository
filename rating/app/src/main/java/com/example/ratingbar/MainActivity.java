b817f5203925eead68ec23a6f68a7c21e555eb25
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-06-16 22:37:35 +0600

    Rating Bar

---
+++ rating/app/src/main/java/com/example/ratingbar/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ratingbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingbar);
+    }
+
+    public void submitRating(View view) {
+        String rating = String.valueOf(ratingBar.getRating());
+        Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_SHORT).show();
+    }
+}
