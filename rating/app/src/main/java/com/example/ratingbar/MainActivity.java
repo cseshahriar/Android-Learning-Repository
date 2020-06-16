68f573dd3603126e7fc73866d7e0e6e2fbeec216
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-06-16 23:42:51 +0600

    alert dialog

--- rating/app/src/main/java/com/example/ratingbar/MainActivity.java
+++ rating/app/src/main/java/com/example/ratingbar/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ratingbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitRating(View view) {
         String rating = String.valueOf(ratingBar.getRating());
-        Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_SHORT).show();
+        // Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_SHORT).show();
+
+        // alert
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setTitle("Display Rating");
+        builder1.setMessage(rating);
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
